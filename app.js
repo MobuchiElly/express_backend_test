fc73bbaccadaef2352339e12e7976eaf71d7611f
MobuchiElly <ugwueleazer@gmail.com>
2024-04-02 15:04:17 +0100

    setting up deployment

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const serverless = require("serverless-http");
+const Products = require("../data");
+
+const app = express();
+
+app.get("/api/products/v1", (req, res) => {
+  try {
+    res.status(200).json(Products);
+  } catch (err) {
+    res.status(404).json({ success: false, msg: err });
+  }
+});
+app.get("/", (req, res) => {
+  try {
+    res.status(200).json(Products);
+  } catch (err) {
+    res.status(404).json({ success: false, msg: err });
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`App is running on port ${PORT}`);
+});
